07d36c625fe58b2920f49f815cbd37933f025f8d
Kopachris <kopachris@gmail.com>
2016-02-03 02:41:17 -0700

    added sas.py and updated sas validation test

---
+++ sas.py
@@ -0,0 +1,9 @@
+"""Implement IGT's Slot Accounting System protocol"""
+
+
+class SASGame(object):
+    pass
+
+
+class SASHost(object):
+    pass
--- sas_validate.py
+++ sas_validate.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
 
+"""Generate secure-enhanced ticket validation numbers according to IGT's
+Slot Accounting System protocol.
+"""
+
 def crc(b, seed=0):
     for x in b:
         q = (seed ^ x) & 0o17
         seed = (seed >> 4) ^ (q * 0o10201)
         q = (seed ^ (x >> 4)) & 0o17
         seed = (seed >> 4) ^ (q * 0o10201)
-    return seed
+    return (seed).to_bytes(2, byteorder='little')
 
 
 def validate(v_id, v_seq):
+    """Generate a validation number from seed values"""
+    
     if 0 > v_id >= 2**24:
         raise ValueError("Validation ID too large, %i" % v_id)
     if 0 > v_seq >= 2**24:
@@ -26,4 +32,26 @@ def validate(v_id, v_seq):
     b[1] = a[1]
     b[0] = a[0]
     
+    c = crc(b[:2])
+    c += crc(b[2:4])
+    c += crc(b[4:])
     
+    n = [0, 0]
+    
+    for i, v in enumerate(c[3:]):
+        n[0] += v << (i * 8)
+        
+    for i, v in enumerate(c[:3]):
+        n[1] += v << (i * 8)
+        
+    v = [int(x) for x in '%08i%08i' % tuple(n)]
+    v.reverse()
+    v[7] |= (sum(v[:8]) % 5) << 1
+    v[15] |= (sum(v[8:]) % 5) << 1
+    v.reverse()
+    
+    return '00' + ''.join([str(x) for x in v])
+
+    
+if __name__ == '__main__':
+    print(validate(0x654321, 0x01))
