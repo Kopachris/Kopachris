f3fbf3bcab4cc4982e09da029c38b5016599c0b6
Chris Koch <kopachris@gmail.com>
2016-03-02 23:30:11 -0700

    Added reverse validation algorithm

--- sas_validate.py
+++ sas_validate.py
@@ -51,7 +51,103 @@ def validate(v_id, v_seq):
     v.reverse()
     
     return '00' + ''.join([str(x) for x in v])
+
+
+def reverse_validate(v_num):
+    """Attempt to reverse the validation algorithm to find validation ID and
+    sequence, given a validation number. Validation number must start with '00'
+    and be 18 digits long to be recognized as a secure-enhanced validation
+    number.
+    """
     
+    if len(v_num) != 18 or not v_num.startswith('00'):
+        raise ValueError("Not a valid secure-enhanced validation number")
+        
+    v = [int(x) for x in v_num[2:]]
+    v.reverse()
+    
+    # find original v[7]
+    found = 0
+    x = 0
+    for i in range(10):
+        if (i | ((sum(v[:7]) + i) % 5) << 1) == v[7]:
+            found += 1
+            x = i
+    if found > 1:
+        raise Exception("Found too many values for original v[7]")
+    elif not found:
+        raise Exception("Couldn't find original v[7]")
+    else:
+        v[7] = x
+        
+    # find original v[15]
+    found = 0
+    x = 0
+    for i in range(10):
+        if (i | ((sum(v[8:15]) + i) % 5) << 1) == v[15]:
+            found += 1
+            x = i
+    if found > 1:
+        raise Exception("Found too many values for original v[15]")
+    elif not found:
+        raise Exception("Couldn't find original v[15]")
+    else:
+        v[15] = x
+    
+    v.reverse()
+    
+    n = [0,0]
+    # convert digits to single integer
+    n[0] = sum([x * 10**i for i, x in zip(range(7,-1,-1), v[:8])])
+    n[1] = sum([x * 10**i for i, x in zip(range(7,-1,-1), v[8:])])
+    
+    # convert to bytes
+    c = (n[1]).to_bytes(3, byteorder='little')
+    c += (n[0]).to_bytes(3, byteorder='little')
+    
+    # brute-force original values for CRC
+    b = [0] * 6
+    founda = 0
+    foundb = 0
+    foundc = 0
+    for x in range(256):
+        for y in range(256):
+            r = crc([x, y])
+            if r == c[:2]:
+                founda += 1
+                b[:2] = [x, y]
+            if r == c[2:4]:
+                foundb += 1
+                b[2:4] = [x, y]
+            if r == c[4:]:
+                foundc += 1
+                b[4:] = [x, y]
+    for f in (founda, foundb, foundc):
+        if f > 1:
+            raise Exception("Found CRC collision")
+        elif not f:
+            raise Exception("No match found for CRC")
+    
+    # reverse original XORing
+    a = [0] * 6
+    a[0] = b[0]
+    a[1] = b[1]
+    a[2] = b[2] ^ b[0]
+    a[3] = b[3] ^ b[1]
+    a[4] = b[4] ^ b[0]
+    a[5] = b[5] ^ b[1]
+    
+    # convert back to integers!
+    n = [0,0]
+    for i, x in enumerate(a[:3]):
+        # validation sequence
+        n[1] += x << (i * 8)
+    for i, x in enumerate(a[3:]):
+        # validation ID
+        n[0] += x << (i * 8)
+        
+    return tuple(n)
+
     
 def search(v_num, v_id=None, v_seq=None):
     """Brute-force search for a validation ID or sequence number given
